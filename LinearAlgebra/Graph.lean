c51320829d09dc754fbb5cdaedf32439b93cf45c
alexbyard <alexbyard64@gmail.com>
2023-06-06 12:33:49 -0500

    More graph definitions, the most significant being PairGraph

--- LinearAlgebra/Graph.lean
+++ LinearAlgebra/Graph.lean
@@ -3,7 +3,42 @@ import Mathlib.Data.Finset.Card
 
 open Classical
 
+/-- Graph is a general structure that admits simple graphs, digraphs, multigraphs, and other degenerate
+    flavors as instances. The `inc` constructor takes a boolean value, an edge, and returns a vertex that
+    is the head or tail of that edge. -/
 @[ext]
 structure Graph (V : Type _) (E : Type _) where
   inc : Bool → E → Finset V
   well_def : ∀ i e, 2 ≤ (inc i e).card → ∃ (u v : V), u ≠ v ∧ ∀ j, inc j e = ({u,v} : Finset V)
+
+namespace Graph
+
+variable {V E : Type _} {G : Graph V E} {e f : E} {u v w : V}
+
+def undir (G : Graph V E) (e : E) : Prop :=
+  ∃ (S : Finset V), ∀ i, G.inc i e = S
+
+def loop (G : Graph V E) (e : E) : Prop :=
+  ∃ u, ∀ i, G.inc i e = {u} 
+
+def free (G : Graph V E) (e : E) : Prop :=
+  ∀ i, G.inc i e = ∅
+
+def half_edge (G : Graph V E) (e : E) : Prop :=
+  ∃ i u, G.inc i e = {u} ∧ G.inc (!i) e = ∅
+
+class UndirectedGraph (G : Graph V E) : Prop :=
+  edge_symm : ∀ e, G.undir e
+
+class LooplessGraph (G : Graph V E) : Prop :=
+  no_loops : ∀ e, ¬G.loop e
+  no_free : ∀ e, ¬G.free e
+  no_half : ∀ e, ¬G.half_edge e 
+
+class SimpleGraph (G : Graph V E) extends LooplessGraph G, UndirectedGraph G
+
+def edge_between (G : Graph V E) (e : E) (v₁ v₂ : V) : Prop :=
+  ∀ i, G.inc i e = {v₁,v₂}
+
+class PairGraph (G : Graph V (V × V)) :=
+  h_pair' : ∀ e, G.free e ∨ G.edge_between e e.1 e.2
